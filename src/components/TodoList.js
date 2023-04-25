397644f586f788068eb2c14f7ec97918785a04ff
fatimabatool <fatima.batool@xloopdigital.com>
2023-04-25 06:58:40 -0700

    refactor code completed all requirements with editing function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import TodoItem from './TodoItem';
 
-const TodoList = ({ todos, removeHandler, updateTodo, editHandler }) => (
-    <div>
-      {todos.map((t, i) => (
-        <TodoItem key={i} todo={t} removeHandler={removeHandler} updateTodo={updateTodo} editHandler={editHandler} />
-      ))}
-    </div>
-  );
+const TodoList = ({ todos, removeHandler, updateTodo, toggleCompleted }) => {
+return (
+<div className="todo-list">
+{todos.map((t) => (
+<TodoItem key={t.id} todo={t} removeHandler={removeHandler} updateTodo={updateTodo} toggleCompleted={toggleCompleted} />
+))}
+</div>
+);
+};
 
 export default TodoList;
+
